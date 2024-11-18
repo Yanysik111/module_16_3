fdc53ff34c38607b59565ecf75cf82f8bb8b9ca4
Yanysik111 <yanyisik@yandex.ru>
2024-11-18 17:14:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 from typing import Annotated
 
 app = FastAPI()
 
-@app.get('/')
-async def welcome() -> dict:
-    return {'message': 'Главная страница'}
+users ={'1': 'Имя: Example, возраст: 18'}
 
-@app.get('/user/admin')
-async def admin() -> dict:
-    return {'message': 'Вы вошли как администратор'}
 
-@app.get('/user/{user_id}')
-async def ident(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> dict:
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
-@app.get('/user/{username}/{age}')
-async def users_inform(username: Annotated[str, Path(ge= 5, le=20, description='Enter username', example= 'UrbanUser')],
-                       age: Annotated[int, Path(ge=18, le=120, description='Enter age', example= '24')]) -> dict:
-    return {'massage':f'Информация о пользователе. Имя: {username}, Возраст: {age}.'}
+@app.get('/users')
+async def get_users() -> dict:
+    return users
+
+@app.post('/user/{username}/{age}')
+async def register(username: Annotated[str, Path(min_length= 5, max_length=20, description='Enter username', example= 'UrbanUser')],
+                       age:int = Path(ge=18, le=120, description='Enter age', example= '24')) -> dict:
+    string = str(int(max(users, key=int)) +1)
+    new_message = f"Имя: {username}, возраст: {age}"
+    users[string] = new_message
+    return {"message": f"User {string} is registered"}
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update(username: Annotated[str, Path(min_length= 5, max_length=20, description='Enter username', example= 'UrbanUser')],
+                       age: int = Path(ge=18, le=120, description='Enter age', example= '24'), user_id: int = Path(ge=0)) -> dict:
+    users[user_id] =f" Имя: {username}, возраст: {age}"
+    return {"message": f"The user {user_id} is updated"}
+
+@app.delete('/user/{user_id}')
+async def delete_users(user_id: int = Path(ge=0)):
+    if user_id in users:
+        users.pop(user_id)
+        return f"User {user_id} has been deleted"
+    else:
+        raise HTTPException(status_code=404, detail="Пользователь не найден.")
